2e42f810dfa56207a6ce21763a83c494daaf353e
Svetlana Sych <sychsvetlana@list.ru>
2021-12-24 14:14:06 +0300

    неизвестная ошибка

--- task_3/solution.js
+++ task_3/solution.js
@@ -16,24 +16,20 @@ function renderCartItem(item) {
     // 5 × 500.00 ₽ = 2500 ₽
     // 1. Создайте переменную itemCountText, равную пустой строке
     let itemCountText;
-    console.log(messege.toString());
     // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
     itemCountText = itemCountText + product.count;
     // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
-    let d = ' x ';
-    itemCountText = itemCountText + d;
+    itemCountText = itemCountText + ' x ';
     // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
     itemCountText = itemCountText + product.price;
     // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
-    let e = ' ₽';
-    let k = ' = ';
-    itemCountText = itemCountText + e + k;
+    itemCountText = itemCountText + ' ₽ = ';
     // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
-    let sum = product.count * product.price;
+    let sum = product.count * number(product.price);
     // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     itemCountText = itemCountText + sum;
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
-    itemCountText = itemCountText + e;
+    itemCountText = itemCountText + ' ₽';
     // Конец решения задания №3.1.
 
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
